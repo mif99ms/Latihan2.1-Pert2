74429faf2324f78151814be08f994a7bb67fa4f2
mif99ms <43515183+mif99ms@users.noreply.github.com>
2018-09-25 06:18:32 +0700

    Initial commit

---
+++ src/latihan2/pkg1/pert2/Latihan21Pert2.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package latihan2.pkg1.pert2;
+
+/**
+ *
+ * @author USER99
+ */
+public class Latihan21Pert2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int umur;
+        umur = 5;
+        if (umur >=17) {
+            System.out.println("Kamu udah boleh punya KTP");
+        }else {
+            System.out.println("Kamu Belum Boleh Punya KTP");
+         
+        }
+        
+        System.out.println((umur>=17)?"Kamu udah boleh punya KTP": "Kamu Belum Boleh Punya KTP");
+        
+        
+        }
+    }
+    
+
